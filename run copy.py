5ab73df0492477463522a858d5b019f14aeae1d3
lishoupeng <757187977@qq.com>
2023-06-27 15:04:34 +0800

    init project

---
+++ run copy.py
@@ -0,0 +1,39 @@
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain import OpenAI,VectorDBQA
+from langchain.document_loaders import DirectoryLoader
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceEmbeddings
+
+import langchain
+
+langchain.debug = True
+#import nltk
+# nltk.download('punkt')
+
+# 加载文件夹中的所有txt类型的文件
+loader = DirectoryLoader('/Users/spli/Desktop/workspace@self/LangChain/document', glob='create table sql.txt')
+# 将数据转成 document 对象，每个文件会作为一个 document
+documents = loader.load()
+
+# 初始化加载器
+text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+# 切割加载的 document
+split_docs = text_splitter.split_documents(documents)
+
+# 初始化 openai 的 embeddings 对象
+# embeddings = OpenAIEmbeddings()
+embeddings = HuggingFaceEmbeddings(model_name='shibing624/text2vec-base-chinese')
+
+# 将 document 通过 openai 的 embeddings 对象计算 embedding 向量信息并临时存入 Chroma 向量数据库，用于后续匹配查询
+docsearch = Chroma.from_documents(split_docs, embeddings)
+
+# 创建问答对象
+qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch, return_source_documents=True)
+# 进行问答
+# result = qa({"query": "实例表的表名叫什么"})
+result = qa({"query": "实例表和任务表如何关联？给我个 sql demo"})
+
+# result = qa({"query": "实例表有多少个实例？"})
+print(result)
