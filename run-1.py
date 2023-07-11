6ce33b91f1731772fa72949f341783a7fab7e211
spli <lishoupeng@zhihu.com>
2023-07-11 16:46:13 +0800

    run.py

---
+++ run-1.py
@@ -0,0 +1,9 @@
+import os
+from langchain.llms import OpenAI
+
+# os.environ["OPENAI_API_KEY"] = "****"
+# print(os.environ['OPENAI_API_KEY'])  
+
+llm = OpenAI(model_name="text-davinci-003", max_tokens=1024)
+result = llm("怎么评价人工智能")
+print(result)
